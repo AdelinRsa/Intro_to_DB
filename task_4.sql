05d42a2a2b8e26c6755a0868cc70910590e16256
AdelinRsa <Adelinrsa@gmail.com>
2024-10-20 20:40:44 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,6 @@
-mysql -u your_username -p alx_book_store < task_4.sql
+-- Script to show full description of books table using INFORMATION_SCHEMA
+USE alx_book_store;
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
